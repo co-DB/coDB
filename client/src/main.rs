0401f19cd14d7817ec20bac414a50ed027e3d70c
kTrzcinskii <kt.trzcinski@gmail.com>
2025-09-02 12:53:17 +0200

    Remove dummy tests

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,11 +1,3 @@
 fn main() {
     println!("Hello client!");
 }
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn dummy_test() {
-        assert_eq!(2 + 3, 5);
-    }
-}
--- query/src/lib.rs
+++ query/src/lib.rs
@@ -1,19 +1,4 @@
 pub mod ast;
-
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
 pub mod lexer;
 pub mod parser;
 pub mod tokens;
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
