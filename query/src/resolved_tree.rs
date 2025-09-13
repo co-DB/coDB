4c90cc1c1571c9215086205a1d295c5b8e5f9240
kTrzcinskii <kt.trzcinski@gmail.com>
2025-09-13 14:03:36 +0200

    Add `resolved_type` to get type of any resolved expression + add new node: `ResolvedCast`

--- query/src/resolved_tree.rs
+++ query/src/resolved_tree.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 use metadata::types::Type;
 use time::{Date, PrimitiveDateTime};
 
@@ -63,6 +65,7 @@ pub(crate) enum ResolvedExpression {
     Logical(ResolvedLogicalExpression),
     Binary(ResolvedBinaryExpression),
     Unary(ResolvedUnaryExpression),
+    Cast(ResolvedCast),
     Literal(ResolvedLiteral),
 }
 
@@ -108,6 +111,12 @@ pub(crate) struct ResolvedUnaryExpression {
 }
 
 #[derive(Debug)]
+pub(crate) struct ResolvedCast {
+    pub(crate) child: ResolvedNodeId,
+    pub(crate) new_ty: Type,
+}
+
+#[derive(Debug)]
 pub(crate) enum ResolvedLiteral {
     String(String),
     Float32(f32),
@@ -117,4 +126,59 @@ pub(crate) enum ResolvedLiteral {
     Bool(bool),
     Date(Date),
     DateTime(PrimitiveDateTime),
+}
+
+impl From<&ResolvedLiteral> for Type {
+    fn from(value: &ResolvedLiteral) -> Self {
+        match value {
+            ResolvedLiteral::String(_) => Type::String,
+            ResolvedLiteral::Float32(_) => Type::F32,
+            ResolvedLiteral::Float64(_) => Type::F64,
+            ResolvedLiteral::Int32(_) => Type::I32,
+            ResolvedLiteral::Int64(_) => Type::I64,
+            ResolvedLiteral::Bool(_) => Type::Bool,
+            ResolvedLiteral::Date(_) => Type::Date,
+            ResolvedLiteral::DateTime(_) => Type::DateTime,
+        }
+    }
+}
+
+#[derive(PartialEq, Eq)]
+pub(crate) enum ResolvedType {
+    LiteralType(Type),
+    TableRef,
+}
+
+impl ResolvedExpression {
+    pub(crate) fn resolved_type(&self) -> ResolvedType {
+        match self {
+            ResolvedExpression::TableRef(_) => ResolvedType::TableRef,
+            ResolvedExpression::ColumnRef(cr) => ResolvedType::LiteralType(cr.ty),
+            ResolvedExpression::FunctionRef(resolved_function) => {
+                ResolvedType::LiteralType(resolved_function.output_ty)
+            }
+            ResolvedExpression::Logical(_) => ResolvedType::LiteralType(Type::Bool),
+            ResolvedExpression::Binary(resolved_binary_expression) => {
+                ResolvedType::LiteralType(resolved_binary_expression.ty)
+            }
+            ResolvedExpression::Unary(resolved_unary_expression) => {
+                ResolvedType::LiteralType(resolved_unary_expression.ty)
+            }
+            ResolvedExpression::Literal(resolved_literal) => {
+                ResolvedType::LiteralType(resolved_literal.into())
+            }
+            ResolvedExpression::Cast(resolved_cast) => {
+                ResolvedType::LiteralType(resolved_cast.new_ty)
+            }
+        }
+    }
+}
+
+impl fmt::Display for ResolvedType {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            ResolvedType::LiteralType(ty) => write!(f, "{ty}"),
+            ResolvedType::TableRef => write!(f, "TableRef"),
+        }
+    }
 }
