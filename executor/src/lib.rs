2d33fd76bbd8151ec9c091bc64633dbc46595575
kTrzcinskii <kt.trzcinski@gmail.com>
2025-11-17 18:19:23 +0100

    Split `execute_create_table_statement` into helper functions

--- executor/src/lib.rs
+++ executor/src/lib.rs
@@ -7,13 +7,13 @@ use dashmap::DashMap;
 use engine::{data_types, heap_file::HeapFile, record::Record};
 use itertools::Itertools;
 use metadata::{
-    catalog::{Catalog, ColumnMetadata, TableMetadata},
+    catalog::{Catalog, ColumnMetadata, ColumnMetadataError, TableMetadata},
     types::Type,
 };
 use parking_lot::RwLock;
 use planner::{
     query_plan::{CreateTable, StatementPlan, StatementPlanItem},
-    resolved_tree::ResolvedTree,
+    resolved_tree::{ResolvedCreateColumnDescriptor, ResolvedTree},
 };
 use storage::cache::Cache;
 
@@ -61,7 +61,7 @@ pub enum StatementResult {
 }
 
 impl Executor {
-    pub fn new(database_name: &str) -> Self {
+    pub fn new(database_name: &str, catalog: Catalog) -> Self {
         todo!()
     }
 
@@ -88,45 +88,12 @@ impl Executor {
     }
 
     fn execute_create_table_statement(&self, create_table: &CreateTable) -> StatementResult {
-        let mut column_metadatas = Vec::with_capacity(create_table.columns.len());
-
-        let cols = create_table
-            .columns
-            .iter()
-            .chain(once(&create_table.primary_key_column))
-            .sorted_by(|a, b| {
-                let a_fixed = a.ty.is_fixed_size();
-                let b_fixed = b.ty.is_fixed_size();
-                b_fixed.cmp(&a_fixed)
-            });
-
-        let mut pos = 0;
-        let mut last_fixed_pos = 0;
-        let mut base_offset = 0;
-        for col in cols {
-            let column_metadata = match ColumnMetadata::new(
-                col.name.clone(),
-                col.ty,
-                pos,
-                base_offset,
-                last_fixed_pos,
-            ) {
-                Ok(cm) => cm,
-                Err(err) => {
-                    return StatementResult::RuntimeError {
-                        error: format!("Failed to create column: {}", err),
-                    };
-                }
-            };
-            column_metadatas.push(column_metadata);
-            pos += 1;
-            if col.ty.is_fixed_size() {
-                let offset = data_types::type_size_on_disk(&col.ty)
-                    .expect("type with fixed size must return value");
-                last_fixed_pos += 1;
-                base_offset += offset;
+        let column_metadatas = match self.process_columns(create_table) {
+            Ok(cm) => cm,
+            Err(err) => {
+                return self.runtime_error(format!("Failed to create column: {err}"));
             }
-        }
+        };
 
         let table_metadata = match TableMetadata::new(
             &create_table.name,
@@ -134,21 +101,67 @@ impl Executor {
             &create_table.primary_key_column.name,
         ) {
             Ok(tm) => tm,
-            Err(err) => {
-                return StatementResult::RuntimeError {
-                    error: format!("Failed to create table: {}", err),
-                };
-            }
+            Err(err) => return self.runtime_error(format!("Failed to create table: {}", err)),
         };
 
-        match self.catalog.write().add_table(table_metadata) {
+        let mut catalog = self.catalog.write();
+
+        if let Err(err) = catalog.add_table(table_metadata) {
+            return self.runtime_error(format!("Failed to create table: {}", err));
+        }
+
+        match catalog.sync_to_disk() {
             Ok(_) => StatementResult::OperationSuccessful {
                 rows_affected: 0,
                 ty: StatementType::Create,
             },
-            Err(err) => StatementResult::RuntimeError {
-                error: format!("Failed to create table: {}", err),
-            },
+            Err(err) => {
+                self.runtime_error(format!("Failed to save catalog content to disk: {err}"))
+            }
         }
+    }
+
+    fn runtime_error(&self, msg: String) -> StatementResult {
+        StatementResult::RuntimeError { error: msg }
+    }
+
+    fn sort_columns_by_fixed_size<'c>(
+        &self,
+        create_table: &'c CreateTable,
+    ) -> impl Iterator<Item = &'c ResolvedCreateColumnDescriptor> {
+        create_table
+            .columns
+            .iter()
+            .chain(once(&create_table.primary_key_column))
+            .sorted_by(|a, b| {
+                let a_fixed = a.ty.is_fixed_size();
+                let b_fixed = b.ty.is_fixed_size();
+                b_fixed.cmp(&a_fixed)
+            })
+    }
+
+    fn process_columns(
+        &self,
+        create_table: &CreateTable,
+    ) -> Result<Vec<ColumnMetadata>, ColumnMetadataError> {
+        let mut column_metadatas = Vec::with_capacity(create_table.columns.len());
+
+        let cols = self.sort_columns_by_fixed_size(create_table);
+
+        let mut pos = 0;
+        let mut last_fixed_pos = 0;
+        let mut base_offset = 0;
+
+        for col in cols {
+            let column_metadata =
+                ColumnMetadata::new(col.name.clone(), col.ty, pos, base_offset, last_fixed_pos)?;
+            column_metadatas.push(column_metadata);
+            pos += 1;
+            if let Some(offset) = data_types::type_size_on_disk(&col.ty) {
+                last_fixed_pos += 1;
+                base_offset += offset;
+            }
+        }
+        Ok(column_metadatas)
     }
 }
