a728cbfd7aa01a293634bdbcc3f5f664ed536a71
Aleksander Bielicki <aleksanderbielicki2003@gmail.com>
2025-11-20 18:03:24 +0100

    Add constructor + initializer to CatalogManager

--- metadata/src/catalog_manager.rs
+++ metadata/src/catalog_manager.rs
@@ -1,14 +1,25 @@
 ﻿use crate::consts::METADATA_FILE_NAME;
+use crate::metadata_file_helper::MetadataFileHelper;
 use directories::ProjectDirs;
 use serde::{Deserialize, Serialize};
-use std::path::PathBuf;
+use serde_json::error::Category::Data;
+use std::fs::File;
+use std::path::{Path, PathBuf};
+use std::{fs, io};
+use thiserror::Error;
 
+#[derive(Error, Debug)]
 pub enum CatalogManagerError {
+    #[error("no home directory found")]
     NoValidCatalogDirectory,
+    #[error("io error occurred: {0}")]
+    IoError(#[from] io::Error),
+    #[error("json error occurred: {0}")]
+    JsonError(#[from] serde_json::Error),
 }
+
 pub struct CatalogManager {
     database_list: DatabaseList,
-    path: PathBuf,
 }
 
 impl CatalogManager {
@@ -19,17 +30,50 @@ impl CatalogManager {
                 let base_path = project_dir.data_local_dir().to_path_buf();
                 let path = base_path.join(METADATA_FILE_NAME);
 
-                Ok(CatalogManager {
-                    database_list: DatabaseList { names: Vec::new() },
-                    path,
-                })
+                let result = MetadataFileHelper::latest_catalog_json(&path, |path| {
+                    DatabaseList::read_from_json(path)
+                })?;
+
+                match result {
+                    MaybeLoaded::Loaded(database_list) => Ok(Self { database_list }),
+                    MaybeLoaded::NotFound => {
+                        let list = DatabaseList { names: Vec::new() };
+                        list.write_to_json(&path)?;
+                        Ok(Self {
+                            database_list: list,
+                        })
+                    }
+                }
             }
         }
     }
 }
+
+enum MaybeLoaded {
+    Loaded(DatabaseList),
+    NotFound,
+}
+
 #[derive(Serialize, Deserialize)]
-pub struct DatabaseList {
+struct DatabaseList {
     pub names: Vec<String>,
 }
 
-impl DatabaseList {}
+impl DatabaseList {
+    fn read_from_json(path: impl AsRef<Path>) -> Result<MaybeLoaded, CatalogManagerError> {
+        match fs::read_to_string(path) {
+            Ok(content) => {
+                let value = serde_json::from_str(&content)?;
+                Ok(MaybeLoaded::Loaded(value))
+            }
+            Err(err) if err.kind() == io::ErrorKind::NotFound => Ok(MaybeLoaded::NotFound),
+            Err(err) => Err(err.into()),
+        }
+    }
+
+    fn write_to_json(&self, path: impl AsRef<Path>) -> Result<(), CatalogManagerError> {
+        let content = serde_json::to_string(self)?;
+        fs::write(path, content)?;
+        Ok(())
+    }
+}
