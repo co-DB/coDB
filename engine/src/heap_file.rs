82561a59bb177ee8795ed4296818a5ef10975578
kTrzcinskii <kt.trzcinski@gmail.com>
2025-12-22 12:15:53 +0100

    Fsm improvements

--- engine/src/heap_file.rs
+++ engine/src/heap_file.rs
@@ -63,15 +63,25 @@ struct FreeSpaceMap<const BUCKETS_COUNT: usize, H: BaseHeapPageHeader> {
     file_key: FileKey,
     /// [`PageId`] of next page that should be read by fsm in case of not finding suitable page in [`FreeSpaceMap::buckets`].
     next_page_to_read: Mutex<PageId>,
-    /// Set of all pages that were already read from disk (either directly by FSM or by HeapFile while reading/creating page).
-    /// The purpose of this is to minimize duplicates in FSM.
-    already_read: DashSet<PageId>,
+    /// Set of page IDs currently tracked by this FSM.
+    /// Prevents duplicate entries in buckets.
+    tracked_pages: DashSet<PageId>,
     _page_type_marker: PhantomData<H>,
 }
 
 type FsmPage<H> = SlottedPage<PinnedWritePage, H>;
 
 impl<const BUCKETS_COUNT: usize, H: BaseHeapPageHeader> FreeSpaceMap<BUCKETS_COUNT, H> {
+    /// Maximum number of candidate pages to check per bucket before giving up and trying the next bucket.
+    const MAX_CANDIDATES_PER_BUCKET: usize = 32;
+
+    /// Minimum free space that page should have to be added to FSM.
+    /// We don't want to use pages with less free space, as most of the time they won't be use anyway.
+    const MIN_USEFUL_FREE_SPACE: usize = 32;
+
+    /// Maximum number of pages to load from disk before giving up and allocating new page.
+    const MAX_DISK_PAGES_TO_CHECK: usize = 4;
+
     fn new(cache: Arc<Cache>, file_key: FileKey, first_page_id: PageId) -> Self {
         let buckets: [SegQueue<PageId>; BUCKETS_COUNT] = array::from_fn(|_| SegQueue::new());
 
@@ -80,49 +90,87 @@ impl<const BUCKETS_COUNT: usize, H: BaseHeapPageHeader> FreeSpaceMap<BUCKETS_COU
             cache,
             file_key,
             next_page_to_read: Mutex::new(first_page_id),
-            already_read: DashSet::new(),
+            tracked_pages: DashSet::new(),
             _page_type_marker: PhantomData::<H>,
         }
     }
 
     /// Finds a page that has at least `needed_space` bytes free.
-    ///
-    /// If a candidate is stale the search continues.
-    /// If page was found it is removed from FSM and should be added back once the thread that removed it finished processing it.
-    /// Returns `Ok(None)` if no suitable page is found.
     fn page_with_free_space(
         &self,
         needed_space: usize,
     ) -> Result<Option<(PageId, FsmPage<H>)>, HeapFileError> {
         let start_bucket_idx = self.bucket_for_space(needed_space);
-        let mut insert_back = vec![];
+
         for b in start_bucket_idx..BUCKETS_COUNT {
-            insert_back.clear();
-            while let Some(page_id) = self.buckets[b].pop() {
+            let mut candidates_checked = 0;
+            let mut reinsert_to_current_bucket = Vec::new();
+
+            while candidates_checked < Self::MAX_CANDIDATES_PER_BUCKET {
+                let Some(page_id) = self.buckets[b].pop() else {
+                    break;
+                };
+
+                // Remove from tracked_pages
+                self.tracked_pages.remove(&page_id);
+
+                candidates_checked += 1;
+
+                // Quick check with read lock
                 let key = self.file_page_ref(page_id);
-                let page = self.cache.pin_write(&key)?;
-                let slotted_page = SlottedPage::new(page)?;
-                let actual_free_space = slotted_page.free_space()?;
-                if actual_free_space >= needed_space as _ {
-                    for id in insert_back {
-                        self.buckets[b].push(id);
+                let actual_free_space = {
+                    let read_page = self.cache.pin_read(&key)?;
+                    let slotted_page = SlottedPage::<_, H>::new(read_page)?;
+                    slotted_page.free_space()?
+                } as usize;
+
+                if actual_free_space >= needed_space {
+                    let page = self.cache.pin_write(&key)?;
+                    let slotted_page = SlottedPage::new(page)?;
+                    let final_free_space = slotted_page.free_space()? as usize;
+
+                    // Check if between read and write lock free space didn't change
+                    if final_free_space >= needed_space {
+                        // Before returning reinsert the pages
+                        for id in reinsert_to_current_bucket {
+                            if self.tracked_pages.insert(id) {
+                                self.buckets[b].push(id);
+                            }
+                        }
+                        return Ok(Some((page_id, slotted_page)));
                     }
-                    return Ok(Some((page_id, slotted_page)));
+
+                    // Space was taken between read and write lock
+                    if final_free_space >= Self::MIN_USEFUL_FREE_SPACE {
+                        let correct_bucket = self.bucket_for_space(final_free_space);
+                        if correct_bucket == b {
+                            reinsert_to_current_bucket.push(page_id);
+                        } else if correct_bucket < b {
+                            self.add_to_bucket(page_id, final_free_space);
+                        }
+                    }
+                    continue;
                 }
-                // If we don't use this page and it still belongs to this bucket we must reinsert it to keep it in FSM.
-                // If it now has different bucket it means that it was edited (other thread made insert/delete) and that thread should put it back in fsm.
-                let actual_bucket = self.bucket_for_space(actual_free_space as usize);
-                if actual_bucket == b {
-                    // We cannot insert it right away, because we would end up in infinity loop
-                    insert_back.push(page_id);
+
+                // Page doesn't have enough space for our request
+                if actual_free_space >= Self::MIN_USEFUL_FREE_SPACE {
+                    let correct_bucket = self.bucket_for_space(actual_free_space);
+                    if correct_bucket == b {
+                        reinsert_to_current_bucket.push(page_id);
+                    } else {
+                        self.add_to_bucket(page_id, actual_free_space);
+                    }
                 }
             }
-            for id in &insert_back {
-                self.buckets[b].push(*id);
+
+            // Reinsert pages that belong in this bucket
+            for id in reinsert_to_current_bucket {
+                if self.tracked_pages.insert(id) {
+                    self.buckets[b].push(id);
+                }
             }
         }
 
-        // We didn't find page in current FSM, we fallback to reading directly from disk
         self.page_with_free_space_from_disk(needed_space)
     }
 
@@ -132,62 +180,76 @@ impl<const BUCKETS_COUNT: usize, H: BaseHeapPageHeader> FreeSpaceMap<BUCKETS_COU
         &self,
         needed_space: usize,
     ) -> Result<Option<(PageId, FsmPage<H>)>, HeapFileError> {
-        while let Some((page_id, slotted_page)) = self.load_next_page()? {
-            // It means this page was already in FSM and we already checked it
-            if self.already_read.contains(&page_id) {
-                continue;
+        let mut pages_checked = 0;
+
+        while pages_checked < Self::MAX_DISK_PAGES_TO_CHECK {
+            let Some((page_id, page)) = self.load_next_page()? else {
+                break;
+            };
+
+            pages_checked += 1;
+            let free_space = page.free_space()? as usize;
+
+            if free_space >= needed_space {
+                return Ok(Some((page_id, page)));
             }
-            self.already_read.insert(page_id);
-            let space = slotted_page.free_space()?;
-            if space >= needed_space as _ {
-                return Ok(Some((page_id, slotted_page)));
+
+            // Only add to FSM if it has useful space
+            if free_space >= Self::MIN_USEFUL_FREE_SPACE {
+                self.add_to_bucket(page_id, free_space);
             }
-            self.update_page_bucket(page_id, space as _);
         }
+
         Ok(None)
     }
 
     /// Loads page with id [`Self::next_page_to_read`] and updates it to the next pointer.
     /// Returns id and loaded page or `None` if there is no more page to read.
     fn load_next_page(&self) -> Result<Option<(PageId, FsmPage<H>)>, HeapFileError> {
-        let mut page_id = self.next_page_to_read.lock();
-        if *page_id == H::NO_NEXT_PAGE {
+        let mut next_page_lock = self.next_page_to_read.lock();
+
+        if *next_page_lock == H::NO_NEXT_PAGE {
             return Ok(None);
         }
 
-        let key = self.file_page_ref(*page_id);
+        let page_id = *next_page_lock;
+
+        // Skip pages that are already tracked (already in FSM buckets)
+        if self.tracked_pages.contains(&page_id) {
+            let key = self.file_page_ref(page_id);
+            let read_page = self.cache.pin_read(&key)?;
+            let slotted_page = SlottedPage::<_, H>::new(read_page)?;
+            *next_page_lock = slotted_page.get_header()?.next_page();
+            return self.load_next_page();
+        }
+
+        let key = self.file_page_ref(page_id);
         let page = self.cache.pin_write(&key)?;
         let slotted_page = SlottedPage::<_, H>::new(page)?;
 
-        let read_page_id = *page_id;
-        let next_page = slotted_page.get_header()?.next_page();
-        *page_id = next_page;
+        *next_page_lock = slotted_page.get_header()?.next_page();
 
-        Ok(Some((read_page_id, slotted_page)))
+        Ok(Some((page_id, slotted_page)))
     }
 
-    /// Adds a page id to the bucket corresponding to `free_space` (in bytes).
-    ///
-    /// This is a best-effort, in-memory hint: the page id is pushed into the computed
-    /// bucket so future searches may find it. The authoritative free-space value is in
-    /// the page header.
-    fn update_page_bucket(&self, page_id: PageId, free_space: usize) {
-        let bucket_idx = self.bucket_for_space(free_space);
-        self.buckets[bucket_idx].push(page_id);
-    }
-
-    /// Same as [`Self::update_page_bucket`], but it also inserts `page_id` into [`Self::already_read`].
-    ///
-    /// It should be used by HeapFile when it reads/creates a page.
-    /// The reason why this is a separate function is that using [`Self::already_read`] might require waiting for lock,
-    /// so we should do it only when really needed.
-    fn update_page_bucket_with_duplicate_check(&self, page_id: PageId, free_space: usize) {
-        if self.already_read.contains(&page_id) {
+    /// Adds a page to the appropriate bucket and marks it as tracked.
+    /// Only adds if the page has useful free space and isn't already tracked.
+    fn add_to_bucket(&self, page_id: PageId, free_space: usize) {
+        // Don't track pages with very little free space
+        if free_space < Self::MIN_USEFUL_FREE_SPACE {
             return;
         }
-        self.already_read.insert(page_id);
-        let bucket_idx = self.bucket_for_space(free_space);
-        self.buckets[bucket_idx].push(page_id);
+
+        // Only add if not already tracked
+        if self.tracked_pages.insert(page_id) {
+            let bucket_idx = self.bucket_for_space(free_space);
+            self.buckets[bucket_idx].push(page_id);
+        }
+    }
+
+    /// Adds a page id to the bucket corresponding to `free_space`.
+    fn update_page_bucket(&self, page_id: PageId, free_space: usize) {
+        self.add_to_bucket(page_id, free_space);
     }
 
     /// Computes the bucket index for a given amount of free space (in bytes).
@@ -1685,7 +1747,7 @@ impl<const BUCKETS_COUNT: usize> HeapFile<BUCKETS_COUNT> {
     {
         let heap_page = self.read_heap_page::<H>(page_id)?;
         let free_space = heap_page.page.free_space()?;
-        fsm.update_page_bucket_with_duplicate_check(page_id, free_space as _);
+        fsm.update_page_bucket(page_id, free_space as _);
         Ok(heap_page)
     }
 
@@ -1890,7 +1952,7 @@ impl<const BUCKETS_COUNT: usize> HeapFile<BUCKETS_COUNT> {
         H: BaseHeapPageHeader,
     {
         page.update(slot, data)?;
-        fsm.update_page_bucket_with_duplicate_check(page_id, page.page.free_space()? as _);
+        fsm.update_page_bucket(page_id, page.page.free_space()? as _);
         Ok(())
     }
 
@@ -6061,7 +6123,7 @@ mod tests {
         let factory = create_test_heap_file_factory(cache.clone(), file_key.clone());
         let heap_file = Arc::new(factory.create_heap_file().unwrap());
 
-        let num_threads = 16;
+        let num_threads = 10;
         let records_per_thread = 3000;
         let total_records = num_threads * records_per_thread;
 
@@ -6116,6 +6178,10 @@ mod tests {
                             failed_clone.fetch_add(1, Ordering::Relaxed);
                         }
                     }
+
+                    // if i > 0 && i % 100 == 0 {
+                    //     println!("[{thread_id}] at {i}");
+                    // }
                 }
 
                 local_ptrs
